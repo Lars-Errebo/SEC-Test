3a2cdcaf3019f9130bf6e03f311468436d4231c5
Lars Errebo <lse@errebo-kolding.dk>
2014-03-15 15:21:04 +0100

    Added DB connect function

--- DBCON-CLASS.php
+++ DBCON-CLASS.php
@@ -7,8 +7,19 @@ class DBconnect {
 	}
 
 	// some method
-	public function method($something) {
-		echo $something; 		
+	public function DBCON() {
+		
+		/* Login variables */
+
+       define('DB_HOST', 'localhost');
+       define('DB_USER', 'root');
+       define('DB_PASSWORD', 'mr27021');
+       define('DB_DATABASE', 'SEC-Test');
+
+        /* Connect to the database */
+        $db = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD) or die("Connection Error: " . mysql_error());
+        mysql_select_db(DB_DATABASE) or die("Error connecting to db."); 
+			
 	}
 
 	// the deconstruct
---
+++ index.php
@@ -0,0 +1,11 @@
+<?PHP
+
+	include_once('DBCON-CLASS.php');
+	
+	$CON = new DBconnect;
+	
+	$CON->DBCON();
+	
+
+
+?>
